3c12679f092189609841d5c5aba0920f0728c58e
jerryllorenz90-dot <jerryllorenz90@gmail.com>
2025-12-02 11:21:30 -0500

    Delete backend/backend/backend/routes/backend/routes directory

--- backend/backend/backend/routes/backend/routes/backend/routes/backend/routes/Create folder: backend/utils/backend/utils/fetchPrice.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export async function fetchPrice(symbol) {
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd`;
-  const response = await axios.get(url);
-  return response.data[symbol].usd;
-}
